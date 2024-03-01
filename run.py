6293ed47e32867b95915cb1ae2783c5c85409a49
stdhans <stdhans@163.com>
2024-03-01 22:26:28 +0800

    add DataManagerApp to run.py

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ from vnpy.trader.ui import MainWindow, create_qapp
 from vnpy.gateway.ctp import CtpGateway
 from vnpy.app.cta_strategy import CtaStrategyApp
 from vnpy.app.cta_backtester import CtaBacktesterApp
+from vnpy.app.data_manager import DataManagerApp
+
 
 def main():
     """Start VN Trader"""
@@ -15,6 +17,7 @@ def main():
     main_engine.add_gateway(CtpGateway)
     main_engine.add_app(CtaStrategyApp)
     main_engine.add_app(CtaBacktesterApp)
+    main_engine.add_app(DataManagerApp)
 
     main_window = MainWindow(main_engine, event_engine)
     main_window.showMaximized()
